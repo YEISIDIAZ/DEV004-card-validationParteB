f19a34a321c3f9a47c08cbc1ae2be7af74c4a6d6
YEISIDIAZ <yurleidydiaz01.1@gmail.com>
2023-01-30 22:25:30 +0100

    TC PARTE B LISTA

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 import validator from './validator.js';
 
-console.log(validator);
+    let cardNumber = "";   // asignamos a la variable un vacio
+   
+    let btnValidar = document.getElementById("btnValidar"); // asignamos a la variable true o false
+ 
+    let btnInicio = document.getElementById("btnInicio"); // asignamos a la variable true o false 
+  
+   
+    btnValidar.addEventListener("click", () => {                               // si hago clic en el boton, ejecuto la funcion 
+   
+    cardNumber = document.getElementById("cardNumber").value;
+    console.log("var cardNumber del index.js es = " + cardNumber); // concatenacion
+    //console.log("variable cardNUmber del index.js: " + cardNumber)
+    
+    let validation = validator.isValid(cardNumber);
+    console.log("variable validation del index.js: " + validation)
+
+    let maskify = validator.maskify(cardNumber);
+    console.log("variable MASKIFY del index.js: " + maskify)
+
+
+    //let mostrarTC = maskify;
+    // console.log("variable MOSTRAR del index.js: " + mostrarTC)
+    // const mostrarTC = element.innerHTML;
+    // document.getElementById("cardNumber").value = maskify.innerHTML; 
+
+    let newCardNumber= Number(cardNumber);
+    console.log("NUEVA variable NUMBER del index.js: " + newCardNumber)
+
+   
+        if(cardNumber.length < 13 || cardNumber.length > 16){
+            alert("Ingresa un número de tarjeta válido.");
+            }else if(cardNumber === 0){
+                alert("Ingresa un número de tarjeta válido.");
+
+                    }else if(validation===true){
+                        //document.getElementById('cardNumber').style.display = 'none';
+                            console.log(" entramos a maskify-------")
+                           
+                            alert("Tarjeta " + maskify + " válida.");
+                           
+                              document.getElementById("resultado").style.display = "block";
+                            
+                          
+                          
+                    }else {
+                             alert("Tarjeta "+ maskify + " inválida.")
+                         }
+
+
+ });
+
+btnInicio.addEventListener("click", ()=>{ // si hago clic en el boton voy a inicio.
+     document.getElementById("resultado").style.display = "none";
+     document.getElementById("validador").style.display = "block";
+});
+//5352621771856289                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,74 @@
 const validator = {
-  // ...
-};
+       
+  isValid : (cardNumber) => { //objeto, recibe parametro con cardNumber//
+
+    //2538
+    //[2,5,3,8]
+    //[8,3,5,2]
+      let convert = cardNumber.split('').reverse();
+      //El método split() divide un objeto de tipo String en un array (vector) de cadenas
+      //El método reverse() invierte el orden de los elementos de un array
+      // El primer elemento pasa a ser el último y el último pasa a ser el primero.
+
+      console.log("variable convert del validator.js  --->  " + convert)
+      
+
+      let sum     = 0;
+      let n       = 0;
+
+      for (let i = 0; i < convert.length; i++) {  
+        //i=4
+                                // posiciones de la variable
+         //console.log("entro al for del validator.js ")
+
+          if (i % 2 === 1) {
+              n = (convert[i] * 2); //
+
+              //8*2
+              //n=16
+
+              if (n >= 10) {
+                  n = (n + 10) + 1  //27
+                  console.log("VALOR DE LA N en el >10 = " + n)
+              }
+          } else {
+              n = (convert[i])   // n= 9
+          }
+
+          sum = sum + n  //sum = 9
+
+          let sump = sum;
+          console.log("VALOR DE LA N = " + n)
+          console.log("VALOR DE LA TARJETA EN NUMEROS = " + sump)
+
+          //i++
+       }
+       console.log("aqui abajo resultado vector suma**************")
+    console.log(sum)
+     return sum % 10 === 0 ? true :false
+         
+  },
+
+
+
+  maskify : (cardNumber) => {
+      let arr    = cardNumber.split('');   //arr=[5,3,5,2,6,2]
+      //El método split() divide un objeto de tipo String en un array (vector) de cadenas
+      let string = '';
+
+      for (let i = 0; i < arr.length; i++) { //i=6
+          if (i < arr.length - 4) {  //6-4 = 2
+              string = string + '#';   // string=[#,#,  ]
+              
+          } else {
+              string = string + arr[i];  //string=[#,#,5,2,6,2]
+          }
+      }
+      console.log("el string es=== " + string)
+
+      return string;
+  }
+}
 
 export default validator;
+//5352621771856289   
